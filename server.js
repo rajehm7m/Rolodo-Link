4f23efcbe7fe81c70971c5c265c3efcc5e629cc2
rajehm7m <rajehmm7m@gmail.com>
2026-08-12 00:58:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -46,3 +46,10 @@ app.use(express.static(__dirname));
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
